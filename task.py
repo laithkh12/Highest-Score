bd6a2fadb91fa0cd50fc61d52c1eeb519fb0626f
laith12khater <laithkhater12@gmail.com>
2024-11-01 23:26:44 +0200

    code added

---
+++ task.py
@@ -0,0 +1,26 @@
+student_score = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86]
+
+# Option 1
+# totalExamScore = sum(student_score)
+# print(f'The sum is {totalExamScore}')
+
+# Option 2
+# sum = 0
+# for score in student_score:
+#     sum += score
+#
+# print(sum)
+
+# <!---------------------------------------------------------------------------------------!>
+
+# Max number
+# Option 1
+# print(max(student_score))
+
+# Option 2
+# maxScore = 0
+# for score in student_score:
+#     if score > maxScore:
+#         maxScore = score
+#
+# print(maxScore)
